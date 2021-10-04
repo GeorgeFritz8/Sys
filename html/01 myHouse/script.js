db88ee7fc1ef57284c5c4b7fe5eabff60b8d2af5
GeorgeFritz8 <30572@ma-web.nl>
2021-10-04 15:02:28 +0200

    opdrachten gefixed

--- html/01 myHouse/script.js
+++ html/01 myHouse/script.js
@@ -4,11 +4,5 @@ const context = canvas.getContext('2d');
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-let myArray = [];
-
 canvas.width = width;
 canvas.height = height;
-
-let background = new GraphBackGround();
-background.alpha = 1;
-background.draw();
